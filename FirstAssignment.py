55e4036f2072ff998a71fdcf66fbbebdd5bd4f6b
Andrew-Boulos <156961307+Andrew-Boulos@users.noreply.github.com>
2024-01-24 17:12:57 -0800

    hi

--- FirstAssignment.py
+++ FirstAssignment.py
@@ -1,2 +1,30 @@
-print("help")
-print("wiahdkha")
+x = input("Please enter your name person 1")
+y = input(int("please enter your lunch menu cost"))
+z = input(int("please enter your drink cost"))
+
+w = input("Please enter your name person 2")
+e = input(int("please enter your lunch menu cost"))
+q = input(int("please enter your drink cost"))
+
+o = input("Please enter your name person 3")
+u = input(int("please enter your lunch menu cost"))
+p = input(int("please enter your drink cost"))
+
+
+j = (y + z) * 1.1
+
+l = (e + q) * 1.1
+
+m = (u + p) * 1.1
+
+v = (j + l + m) * 1.2
+
+print(x, "'s total is", j)
+
+print(w, "'s total is", l)
+
+print(o, "'s total is", m)
+
+print(" your total is", v)
+
+
