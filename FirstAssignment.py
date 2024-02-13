784efe031bed57a9bbad0f1b5122234d7213792c
Andrew-Boulos <156961307+Andrew-Boulos@users.noreply.github.com>
2024-02-13 13:03:49 -0800

    kgcdfhfgjkhhk lhk

--- FirstAssignment.py
+++ FirstAssignment.py
@@ -1,3 +1,6 @@
+
+
+
 x = input("Please enter your name person 1 ")
 y = input("please enter your lunch menu cost ")
 z = input("please enter your drink cost ")
@@ -39,6 +42,16 @@ print("The total cost is " + str(v))
 
 
 
+
+
+
+#code 2
+
+
+
+
+
+
 e1 = input("employee one enter your name ")
 e1s = input("enter your salary ")
 
@@ -72,3 +85,58 @@ print("employee 5: " + e5 + ", salary: " + e5s)
 
 
 print("Total salary: " + str(ts))
+
+
+
+
+
+
+
+
+
+
+
+
+
+#code 3
+
+
+
+
+
+
+
+
+
+
+
+
+movie = input("Enter movie name: ")
+
+
+
+n_a = int(input("Enter number of adult tickets sold: "))
+n_c = int(input("Enter number of children sold. "))
+
+
+print("THE FRICKING MOVIE NAME IS " + '"' + movie + '"')
+print("Adult tickets sold: " + str(n_a) )
+print("N u m b e r    o f    c h i l d r e n    s o l d     i s :    " + str(n_c) + "!!!!")
+
+y = int(n_a * 6)
+z = int(n_c * 3)
+
+b = float(y + z)
+
+print("Total money: $" + str(b))
+
+paid = float(b * (80/100))
+
+print("Money paid to the very greedy owner: $" + str("%.2f" % paid))
+
+vb = float(b - paid)
+
+print("MONEY FRICKIN KEPT: $" + str("%.2f" % vb))
+
+
+
